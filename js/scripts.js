a7eb8e9e05f26ee5d375a9f0b3c47b871136c931
UC44 <102481644+UC44@users.noreply.github.com>
2022-05-22 00:06:56 -0700

    JS, adding pokemonList

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+let pokemonList = [
+  {name:'Bulbasaur',height: '17.07', types: ['grass','fire']},
+  {name:'Metapod',height:'0.7',types: 'bug'},
+  {name:'Beedrill',height:'1.0',type: ['bug','poison']},
+  {name: 'Nidoran',height:'0.4',type:['Poison','Ground']},
+];
