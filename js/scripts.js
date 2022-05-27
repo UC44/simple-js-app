f7035d0f248500a31c1aa60f2144c19ab2d31f32
UC44 <102481644+UC44@users.noreply.github.com>
2022-05-27 11:17:02 -0700

    JS=N/A

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 let pokemonList = [
-  {name:'Bulbasaur',height: '17.07', types: ['grass','fire']},
-  {name:'Metapod',height:'0.7',types: 'bug'},
-  {name:'Beedrill',height:'1.0',type: ['bug','poison']},
-  {name: 'Nidoran',height:'0.4',type:['Poison','Ground']},
+  { name: 'Bulbasaur', height: '17.07', types: ['grass', 'fire'] },
+  { name: 'Metapod', height: '0.7', types: 'bug' },
+  { name: 'Beedrill', height: '1.0', type: ['bug', 'poison'] },
+  { name: 'Nidoran', height: '0.4', type: ['Poison', 'Ground'] },
 ];
+
+/* For Loops*/
+// for ([initialization]; [condition]; [final-expression]) {}
+// Add always [i] behind the object variable.
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + '' + 'height is' + pokemonList[i].height + 'cm!');
+}
+
+if (pokemonList[i].height >= 17.7) {
+  document.writer('  -I\'m size XXL. Wow, that\'s big!' + '<br>');
+} else if (pokemonList[i].height >= 0.7 && pokemonList[i].height <= 1.1) {
+  document.write('  -I\'m size XL. ' + '<br>');
+} else {
+  document.write('  -I\'m size M. ' + '<br>');
+}
