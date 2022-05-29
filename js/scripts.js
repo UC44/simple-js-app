e8d3e7f3292b3d9b9d312ab25924aaff6a69acfe
UC44 <102481644+UC44@users.noreply.github.com>
2022-05-29 05:50:45 -0700

    JS= Amended Arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList = [
   { name: 'Bulbasaur', height: '17.07', types: ['grass', 'fire'] },
-  { name: 'Metapod', height: '0.7', types: 'bug' },
+  { name: 'Metapod', height: '0.7', types: ['bug'] },
   { name: 'Beedrill', height: '1.0', type: ['bug', 'poison'] },
   { name: 'Nidoran', height: '0.4', type: ['Poison', 'Ground'] },
 ];
@@ -9,13 +9,13 @@ let pokemonList = [
 // for ([initialization]; [condition]; [final-expression]) {}
 // Add always [i] behind the object variable.
 for (let i = 0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + '' + 'height is' + pokemonList[i].height + 'cm!');
-}
+  document.write('<p>' + pokemonList[i].name + 'height is' + pokemonList[i].height + '</p>');
 
-if (pokemonList[i].height >= 17.7) {
-  document.writer('  -I\'m size XXL. Wow, that\'s big!' + '<br>');
-} else if (pokemonList[i].height >= 0.7 && pokemonList[i].height <= 1.1) {
-  document.write('  -I\'m size XL. ' + '<br>');
-} else {
-  document.write('  -I\'m size M. ' + '<br>');
+  if (pokemonList[i].height >= 17) {
+    document.write('  -I\'m size XXL. Wow, that\'s big!');
+  } else if (pokemonList[i].height >= 0.7 && pokemonList[i].height <= 1.1) {
+    document.write('  -I\'m size XL. ');
+  } else {
+    document.write('  -I\'m size X. ');
+  }
 }
