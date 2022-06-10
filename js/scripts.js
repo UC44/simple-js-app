946df0bc8bb592eb9fed2551c1f231c1d9c48880
UC44 <102481644+UC44@users.noreply.github.com>
2022-06-10 14:39:29 -0700

    JS, Adding forEach and IIFE in JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,40 @@
-let pokemonList = [
-  { name: 'Bulbasaur', height: '17.07', types: ['grass', 'fire'] },
-  { name: 'Metapod', height: '0.7', types: ['bug'] },
-  { name: 'Beedrill', height: '1.0', type: ['bug', 'poison'] },
-  { name: 'Nidoran', height:'0.4', type: ['Poison', 'Ground'] },
-];
 
-/* For Loops*/
-// for ([initialization]; [condition]; [final-expression]) {}
-// Add always [i] behind the object variable.
-for (let i = 0; i < pokemonList.length; i++) {
-  document.write( pokemonList[i].name + ' height is '  + pokemonList[i].height);
+let pokemonList = (function () {
+  let pokemonList = [
+    { name: 'Bulbasaur', height: '17.07', types: ['grass', 'fire'] },
+    { name: 'Metapod', height: '0.7', types: ['bug'] },
+    { name: 'Beedrill', height: '1.0', type: ['bug', 'poison'] },
+    { name: 'Nidoran', height: '0.4', type: ['Poison', 'Ground'] },
+  ];
 
-  if (pokemonList[i].height >= 17) {
-    document.write('  -I\'m size XXL. Wow, that\'s big! <br>');
-  } else if (pokemonList[i].height >= 0.7 && pokemonList[i].height <= 1.1) {
-    document.write('  -I\'m size XL. <br>');
-  } else {
-    document.write('  -I\'m size X. <br>');
+  /* For Loops*/
+  // for ([initialization]; [condition]; [final-expression]) {}
+  // Add always [i] behind the object variable.
+  for (let i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[item].name + ' height is ' + pokemonList[item].height);
+
+    if (pokemonList[item].height >= 17) {
+      document.write('  -I\'m size XXL. Wow, that\'s big! <br>');
+    } else if (pokemonList[item].height >= 0.7 && pokemonList[item].height <= 1.1) {
+      document.write('  -I\'m size XL. <br>');
+    } else {
+      document.write('  -I\'m size X. <br>');
+    }
   }
-}
+
+  function getFullname(person) {
+    return (person.firstname + '' + person.lastNmae);
+  }
+
+  let Character = { firstName: 'Homer', lastName: 'Simpson' }
+
+  let fullName = getFullname(Character);
+
+  console.log(fullName);
+
+  pokemonList.forEach(item => {
+    console.log(item.name)
+  });
+
+});
+
