4f4ab46ddfe52dc040e7084883c7077d61e2db7a
UC44 <102481644+UC44@users.noreply.github.com>
2022-06-28 16:57:33 -0700

    Created addListItem

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,23 @@ let pokemonRepository = (function () {
   function add(pokemon) {
     return pokemonList.push(pokemon);
   }
+  function addlistItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listpokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button-class');
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+    button.addEventListener('click', function (event) {
+      showDetails(pokemon);
+    })
+  }
 
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addlistItem: addlistItem
   }
 })();
 
@@ -29,5 +42,36 @@ let newPokemon = { name: 'Irvin', height: '29.87', types: ['air', 'water'] };
 pokemonRepository.add(newPokemon);
 console.log(pokemonRepository.getAll()); // returns all pokemonList with the newly pushed pokemon
 
-pokemonRepository.getAll().forEach(writePokemon);
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addlistItem(pokemon);
+});
+function showDetails(pokemon) {
+  pokemonRepository.loadDetails(pokemon).then(function () {
+    showModal(pokemon);
+  });
 
+}
+// let container = document.querySelector('.container');
+// let button = document.createElement('button');
+// button.innerText = "Click Me";
+// container.appendChild(button);
+
+// let elementToRemove = document.querySelector('p');
+// elementToRemove.parentElement.removeChild(elementToRemove);
+// let mainTitle = document.querySelector('h1');
+// console.log(mainTitle.innerText);
+// mainTitle.innerText = "Hello world";
+// console.log(mainTitle.innerText);
+// let container = document.querySelector('.container');
+// let elementToRemove = document.querySelector('p');
+// elementToRemove.parentElement.removeChild
+// (elementToRemove);
+// let element = document .querySelector('.container');
+// console.log(element.getAttribute('id'));
+// console.log(element.hasAttribute('value'));
+// element.setAttribute('id','new-id');
+// console.log(element.getAttribute('id'));
+// element.classList.contains('container');
+// element.classList.add('my-class');
+// element.classList.remove('my-class');
+// element.classList.toggle('my-class');
